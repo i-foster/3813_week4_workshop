ee64998509e07a456a1ad7efdb511fc231bcea8b
i-foster <nakor27@live.com>
2024-08-20 15:23:17 +1000

    added check for cuser in local storage or redirect to login

--- src/profile/profile.component.ts
+++ src/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LogonService } from '../services/logon.service';
 import { Router } from '@angular/router';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
@@ -13,8 +14,26 @@ export class ProfileComponent  {
   constructor(private router:Router, private localstore:LogonService){ 
   }
 
+
   ngOnInit():void {
-    //localStorage.
+    this.loggedin()
+  }
+    
+  loggedin(){
+    if (localStorage.getItem("cuser") == null){
+      this.router.navigateByUrl("/login")
+    }else {
+      
+    let us:any = localStorage.getItem("cuser")
+    console.log(us);
+
+    let user = (JSON.parse(us))
+    console.log(user.username)
+
+    if (user.valid == true){
+      console.log("true");
+    } 
+  }
   }
 
 
